7f15a0b57c4ea654241685d529dc5751a6529da9
Hakob-developer-cod <hakobziroyanarm@gmail.com>
2026-04-02 21:02:02 +0400

    Add translations for English, Armenian, and Russian

---
+++ translation.js
@@ -0,0 +1,18 @@
+const translations = {
+
+en:{
+title:"Welcome to my website",
+description:"This website supports multiple languages."
+},
+
+am:{
+title:"Բարի գալուստ իմ կայք",
+description:"Այս կայքը աջակցում է մի քանի լեզուների։"
+},
+
+ru:{
+title:"Добро пожаловать на мой сайт",
+description:"Этот сайт поддерживает несколько языков."
+}
+
+};
